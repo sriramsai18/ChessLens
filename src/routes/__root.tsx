acf3e03f423e8f92815959c07f76ea81d251dce6
SRIRAM SAI <119840345+sriramsai18@users.noreply.github.com>
2026-07-08 23:57:20 +0530

    nc6

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -89,7 +89,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/assets/knight-mark.png", type: "image/x-icon" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
